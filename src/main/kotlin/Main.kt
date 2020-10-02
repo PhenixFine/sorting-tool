032bddbf8b363c54e98d7b0f0bdc9301ec291928
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-10-02 17:17:15 -0400

    Stage 2 of 6 for JetBrains Academy - Kotlin - Sorting Tool project. • for stage 2 taking command line arguments was added, if no argument is passed, it defaults to receiving words from the user. With command lines, the user can specify, "word", "long" and "line". When the command is "word", words() is called, "line", lines() is called, "long", long() is called. • numbers() - pulls the number from the lines list, and prints the total numbers, greatest number, count of the greatest number, and percent of the number in the list. • words() - pulls the words from the lines list. Prints total words, the longest word, number of times the word is used, and the percent the word is of total words. • lines() - prints the total lines in the lines list, the longest line, number of times the line is repeated, and percent the line is of the total lines.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,46 @@
-fun main() {
+fun main(args: Array<String>) {
+    val command = if (args.isNotEmpty() && args.size == 2 && args[0] == "-dataType") args[1] else "word"
     val input = generateSequence(::readLine)
     val lines = input.toList()
-    val numbers = mutableListOf<Int>()
+
+    when (command) {
+        "long" -> numbers(lines)
+        "line" -> lines(lines)
+        "word" -> words(lines)
+    }
+}
+
+fun numbers(lines: List<String>) {
+    val numbers = mutableListOf<Long>()
     val max = { numbers.maxOrNull() ?: throw NumberFormatException() }
+    val count = { numbers.count { it == max() } }
 
     for (line in lines) {
-        line.replace("\\s+".toRegex(), " ").split(" ").map { it.toInt() }.forEach { numbers.add(it) }
+        line.replace("\\s+".toRegex(), " ").split(" ").map { it.toLong() }.forEach { numbers.add(it) }
     }
-
     println("Total numbers: ${numbers.size}.")
-    println("The greatest number: ${max()} (${numbers.count { it == max() }} time(s)).")
+    println("The greatest number: ${max()} (${count()} time(s), ${count() * 100 / numbers.size}%).")
+}
+
+fun words(lines: List<String>) {
+    var words = mutableListOf<String>()
+    val max = { words.maxByOrNull { it.length } ?: throw Exception() }
+    val count = { words.count { it == max() } }
+
+    for (line in lines) {
+        line.replace("\\s+".toRegex(), " ").split(" ").map { it }.forEach { words.add(it) }
+    }
+    words = words.sorted().toMutableList()
+    println("Total words: ${words.size}.")
+    println("The longest word: ${max()} (${count()} time(s), ${count() * 100 / words.size}%).")
+}
+
+fun lines(lines: List<String>) {
+    val max = { lines.maxByOrNull { it.length } ?: throw Exception() }
+    val count = { lines.count { it == max() } }
+
+    println("Total lines: ${lines.size}.")
+    println("The longest line:")
+    println(max())
+    println("(${count()} time(s), ${count() * 100 / lines.size}%).")
 }
