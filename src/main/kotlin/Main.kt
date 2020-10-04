7257f9bf1d54e2b50ae3f2280a5c96a917b5a5ef
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-10-04 14:55:01 -0400

    Stage 3 of 6 for JetBrains Academy - Kotlin - Sorting Tool project. • main() - added a check for the command "-sortIntegers" which overrides anything else that may be typed in the command line. val input and lines was merged ( I tried to merge them before, but I must have typed something wrong, as I couldn't get it to work before ). • numbers() - was adjusted for what's printed after total numbers, dependent on if passed val sort is true or not. Working like previous stage if it is false, and using mergeSort() to sort the numbers if it's true and then printing the sorted numbers. • filterLine() pulled the filtering of spaces from words() and numbers() to this function. • mergeSort() - sorts a given Array of Long using recursion and merge(). I don't think it was required to do this, so I'll be removing it in the next stage ( the last topic was about merge sort, so I thought when the stage description said to sort the numbers it meant to implement a version of merge sort, but after re-reading the description and seeing other's code, I don't think I needed to, and that I could just have used a built in sort method.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,30 @@
 fun main(args: Array<String>) {
-    val command = if (args.isNotEmpty() && args.size == 2 && args[0] == "-dataType") args[1] else "word"
-    val input = generateSequence(::readLine)
-    val lines = input.toList()
+    val sort = args.contains("-sortIntegers")
+    val command =
+        if (sort) "long" else if (args.isNotEmpty() && args.size == 2 && args[0] == "-dataType") args[1] else "word"
+    val lines = generateSequence(::readLine).toList()
 
     when (command) {
-        "long" -> numbers(lines)
+        "long" -> numbers(lines, sort)
         "line" -> lines(lines)
         "word" -> words(lines)
     }
 }
 
-fun numbers(lines: List<String>) {
+fun numbers(lines: List<String>, sort: Boolean) {
     val numbers = mutableListOf<Long>()
     val max = { numbers.maxOrNull() ?: throw NumberFormatException() }
     val count = { numbers.count { it == max() } }
 
-    for (line in lines) {
-        line.replace("\\s+".toRegex(), " ").split(" ").map { it.toLong() }.forEach { numbers.add(it) }
-    }
+    for (line in lines) filterLine(line).map { it.toLong() }.forEach { numbers.add(it) }
     println("Total numbers: ${numbers.size}.")
-    println("The greatest number: ${max()} (${count()} time(s), ${count() * 100 / numbers.size}%).")
-}
-
-fun words(lines: List<String>) {
-    var words = mutableListOf<String>()
-    val max = { words.maxByOrNull { it.length } ?: throw Exception() }
-    val count = { words.count { it == max() } }
-
-    for (line in lines) {
-        line.replace("\\s+".toRegex(), " ").split(" ").map { it }.forEach { words.add(it) }
+    if (!sort) println("The greatest number: ${max()} (${count()} time(s), ${count() * 100 / numbers.size}%).") else {
+        val numSort = numbers.toTypedArray()
+        mergeSort(numSort)
+        print("Sorted data: ")
+        for (i in numSort.indices) print("${numSort[i]}" + if (i != numSort.lastIndex) " " else "")
+        println()
     }
-    words = words.sorted().toMutableList()
-    println("Total words: ${words.size}.")
-    println("The longest word: ${max()} (${count()} time(s), ${count() * 100 / words.size}%).")
 }
 
 fun lines(lines: List<String>) {
@@ -43,4 +35,61 @@ fun lines(lines: List<String>) {
     println("The longest line:")
     println(max())
     println("(${count()} time(s), ${count() * 100 / lines.size}%).")
+}
+
+fun words(lines: List<String>) {
+    var words = mutableListOf<String>()
+    val max = { words.maxByOrNull { it.length } ?: throw Exception() }
+    val count = { words.count { it == max() } }
+
+    for (line in lines) filterLine(line).map { it }.forEach { words.add(it) }
+    words = words.sorted().toMutableList()
+    println("Total words: ${words.size}.")
+    println("The longest word: ${max()} (${count()} time(s), ${count() * 100 / words.size}%).")
+}
+
+fun filterLine(line: String) = line.replace("\\s+".toRegex(), " ").split(" ")
+
+// modified from https://www.geeksforgeeks.org/merge-sort/
+fun mergeSort(numbers: Array<Long>, left: Int = 0, right: Int = numbers.lastIndex) {
+    if (left < right) {
+        val middle = (left + right) / 2
+
+        mergeSort(numbers, left, middle)
+        mergeSort(numbers, middle + 1, right)
+        merge(numbers, left, middle, right)
+    }
+}
+
+fun merge(numbers: Array<Long>, left: Int, middle: Int, right: Int) {
+    val sub1 = middle - left + 1
+    val sub2 = right - middle
+    val leftTemp = Array(sub1) { 0L }
+    val rightTemp = Array(sub2) { 0L }
+    var i = 0
+    var j = 0
+    var k = left
+
+    for (i1 in 0 until sub1) leftTemp[i1] = numbers[left + i1]
+    for (j1 in 0 until sub2) rightTemp[j1] = numbers[middle + 1 + j1]
+    while (i < sub1 && j < sub2) {
+        if (leftTemp[i] <= rightTemp[j]) {
+            numbers[k] = leftTemp[i]
+            i++
+        } else {
+            numbers[k] = rightTemp[j]
+            j++
+        }
+        k++
+    }
+    while (i < sub1) {
+        numbers[k] = leftTemp[i]
+        i++
+        k++
+    }
+    while (j < sub2) {
+        numbers[k] = rightTemp[j]
+        j++
+        k++
+    }
 }
